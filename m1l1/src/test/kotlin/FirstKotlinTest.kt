716c8f9fe1177569c3ba93e562fd8844bcf03bea
IUsov <ivan.usov2011@yandex.ru>
2023-04-09 18:46:50 +0300

    [add test]

---
+++ m1l1/src/test/kotlin/FirstKotlinTest.kt
@@ -0,0 +1,11 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FirstKotlinTest {
+
+    @Test
+    fun test(){
+        var company = "Otus"
+        assertEquals(company,"Otus")
+    }
+}
